636800b616e78384f4e490eb4b224478487c01a6
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:58:10 -0700

    initial grid trial

--- static/JavaScript/game.js
+++ static/JavaScript/game.js
@@ -1,1 +1,17 @@
-const grid = document.querySelector('#grid')
+const grid = document.querySelector('#all_boxes')
+let node_boxes = grid.childNodes;
+
+
+for (let r = 1; r < node_boxes.length; r+=2){
+  let this_box = node_boxes[r];
+  this_box.addEventListener("click", clicked(this_player_color, this_box);
+  }
+
+function generateString(box){
+  
+}
+
+function clicked(color, box){
+  box.style.backgroundColor = color;
+  box.innerHtml = '';
+}
