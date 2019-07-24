9d1be19dbe98aebf24a422f9a9b26a2cfea4fa46
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-24 08:13:39 -0700

    first try for text to keyboard to color

--- static/JavaScript/board.js
+++ static/JavaScript/board.js
@@ -1,32 +1,55 @@
-const grid = document.querySelector('#all_boxes')
-let node_boxes = grid.childNodes;
+
+function generateString(box){
+  rand = Math.floor((Math.random() * 25));
+  random_words = ["humorous","star","touch","minister","river","brown","horse","parcel","murmur","choose",
+                  "notice","swot","care","shop","quick","smell","possessive","faulty","disagreeable",
+                  "station","bit","cup","pan","type","obnoxious","abounding", "crave", "jeans", "work",
+                  "ceaseless", "correspond", "seat","superficial", "steadfast", "paint"]
+
+  return word = random_words[rand];
+}
+
+function checkForBox(nodeBoxes, player_color, word){
+  for ( let r = 1; r < nodeBoxes.length; r+=2){
+    let letter = nodeBoxes[r].innerHTML;
+    if (word == letter && nodeBoxes[r].style.backgroundColor!= "black")
+    {
+      changeBoxColor(player_color, nodeBoxes[r]);
+      break;
+    }
+  }
+}
 
 function changeBoxColor(color, box){
   box.style.backgroundColor = color;
   box.innerHtml = '';
 }
 
-window.addEventListener("keydown", event =>{
-  for ( let r = 1; r < node_boxes.length; r+=2)
-  {
-    let letter = node_boxes[r].innerHTML;
-    if (event.key == letter && node_boxes[r].style.backgroundColor!= "black")
-    {
-      changeBoxColor("black", node_boxes[r]);
-      break;
-    }
-  }
-})
+
+
+
+
+
+
+
+const grid = document.querySelector('#all_boxes')
+let node_boxes = grid.childNodes;
 
 for (let r = 1; r < node_boxes.length; r+=2){
   let this_box = node_boxes[r];
   this_box.innerHTML = generateString(node_boxes);
 }
 
-function generateString(box){
-  rand = Math.floor((Math.random() * 25));
-  random_words = ["humorous","star","touch","minister","river","brown","horse","parcel","murmur","choose","notice",
-                  "swot","care","shop","quick","smell","possessive","faulty","disagreeable","station","bit","cup",
-                  "pan","type","obnoxious"]
-  return random_words[rand];
-}
+let temp_string = "";
+
+window.addEventListener("keydown", event =>{
+  if (event.key === "Enter"){
+    checkForBox(node_boxes, "blue", temp_string.toLowerCase());
+    temp_string = "";
+    console.log(temp_string)
+  }
+  else{
+    temp_string += event.key;
+    console.log(temp_string)
+  }
+})
