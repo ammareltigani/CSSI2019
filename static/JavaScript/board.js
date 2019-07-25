5cd06ec8de81939e0a561b362af385644d16ea62
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-25 13:38:02 -0400

    asdfads

--- static/JavaScript/board.js
+++ static/JavaScript/board.js
@@ -1,6 +1,6 @@
 function generateString(){
   random_words = ["humorous","star","touch","minister","river","brown","horse","parcel","murmur","choose",
-                  "notice","swot","care","shop","quick","smell","possessive","faulty","disagreeable",
+                  "notice","swot","care","shop","quick","smell","possessive","faulty",
                   "station","bit", "kiwi", "cup","pan","type","obnoxious","abounding", "crave", "jeans", "city", "work",
                   "ceaseless", "correspond", "seat", "superficial", "steadfast", "paint", "pacify", "snakes",
                   "mighty", "wombat", "elegant", "sneeze", "cap", "police", "fowl", "lead", "idea", "flowery", "huge",
