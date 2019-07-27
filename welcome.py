fa591bcfbb55164d8fe7a5005ede949ec19398e5
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 17:28:08 -0700

    started leaderboard

--- welcome.py
+++ welcome.py
@@ -7,16 +7,12 @@ jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+leaderboard_vars = {}
+
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        username = self.request.get('username')
-        level = self.request.get('level')
-        template_var = {
-        "name": username,
-        "level": level
-        }
         start_template=jinja_env.get_template("templates/welcome.html")
-        self.response.write(start_template.render(template_var))
+        self.response.write(start_template.render())
 
 class GameHandler(webapp2.RequestHandler):
     def post(self):
@@ -28,6 +24,9 @@ class GameHandler(webapp2.RequestHandler):
             "level": level,
             "color":  color,
         }
+        leaderboard_vars["name"] = username
+        leaderboard_vars["level"] = level
+        leaderboard_vars["color"] = color
         game_template=jinja_env.get_template("templates/SquareUp.html")
         self.response.write(game_template.render(template_var))
 
@@ -39,13 +38,13 @@ class HowtoplayHandler(webapp2.RequestHandler):
 class LeaderboardHandler(webapp2.RequestHandler):
     def get(self):
         lead_template=jinja_env.get_template("templates/leaderboard.html")
-        self.response.write(lead_template.render())
+        self.response.write(lead_template.render(leaderboard_vars))
 
 class AboutthecreatorsHandler(webapp2.RequestHandler):
     def get(self):
         start_template=jinja_env.get_template("templates/aboutthecreators.html")
         self.response.write(start_template.render())
-        
+
 app = webapp2.WSGIApplication([
     ('/', WelcomeHandler),
     ('/game', GameHandler),
